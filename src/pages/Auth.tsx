71cbca26ef609783d0dc00e3d4aae89473890ebf
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-02-13 22:18:27 -0300

    feat: add Auth page and fix css import paths

---
+++ src/pages/Auth.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from '@style/Pages/auth.module.css'
+
+export default function Auth() {
+  return (
+    <div className={classes.container}>
+      Auth
+    </div>
+  )
+}

7fc3901af8a8c31d8b405dbd0dc9f5d29e039971
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-02-15 18:01:21 -0300

    feat: add nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
+import classes from './nav.module.css'
 
-export function Nav() {
+type Props = {
+  gadgets: ReactNode
+}
+
+export function Nav({ gadgets }: Props) {
   return (
-    <nav>Nav</nav>
+    <nav className={classes.container}>
+      <form>
+        <button>
+          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8.25 14.25C11.5637 14.25 14.25 11.5637 14.25 8.25C14.25 4.93629 11.5637 2.25 8.25 2.25C4.93629 2.25 2.25 4.93629 2.25 8.25C2.25 11.5637 4.93629 14.25 8.25 14.25Z" stroke="#808080" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M15.75 15.75L12.4875 12.4875" stroke="#808080" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </button>
+        <input type="text" placeholder='Search Notes'/>
+      </form>
+      {gadgets}
+    </nav>
   )
 }
